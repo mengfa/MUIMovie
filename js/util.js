58fe0fc7d09356a8343f2ac04bfd6d4c3e14f825
lvyang <tylvyang@qq.com>
2018-02-03 16:49:22 +0800

    3-2模板调整
    删除多余文件,更改颜色.

--- js/util.js
+++ js/util.js
@@ -2,7 +2,7 @@ var util = {
 	options: {
 		ACTIVE_COLOR: "#007aff",
 		NORMAL_COLOR: "#000",
-		subpages: ["html/tab-webview-subpage-chat.html", "html/tab-webview-subpage-contact.html"]
+		subpages: ["html/main-billboard.html", "html/main-setting.html"]
 	},
 	/**
 	 *  简单封装了绘制原生view控件的方法
